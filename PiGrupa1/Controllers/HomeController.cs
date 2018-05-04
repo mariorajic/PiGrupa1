0117156efab86c7041fbfbf7e461762cb37c86a5
Robert Rozic <rozic.robert@amplius.tech>
2018-05-04 11:57:01 +0200

    Login i registracija forme

--- PiGrupa1/Controllers/HomeController.cs
+++ PiGrupa1/Controllers/HomeController.cs
@@ -14,6 +14,11 @@ namespace PiGrupa1.Controllers
             return View();
         }
 
+        public ActionResult Register()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
