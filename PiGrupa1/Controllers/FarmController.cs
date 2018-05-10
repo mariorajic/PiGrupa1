90e8dcc8df34ff7de60a42902ad19096bf8aea67
mariorajic <mariorajic7@gmail.com>
2018-05-10 12:25:14 +0200

    farm controller & view

---
+++ PiGrupa1/Controllers/FarmController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace PiGrupa1.Controllers
+{
+    public class FarmController : Controller
+    {
+        // GET
+        public ActionResult CreateFarm()
+        {
+            return
+            View();
+        }
+    }
+}
