b8931601ede3d5e007687f75cd64f5bb5eeab785
MateaMilicevic <mateaaa.milicevic@gmail.com>
2018-05-10 13:43:47 +0200

    Evo nesto i od mene

---
+++ PiGrupa1/Controllers/ParcelaController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace PiGrupa1.Controllers
+{
+    public class ParcelaController : Controller
+    {
+        // GET
+        public ActionResult CreateParcela()
+        {
+            return
+            View();
+        }
+    }
+}
