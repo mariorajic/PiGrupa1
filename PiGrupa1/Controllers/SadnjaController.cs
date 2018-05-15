4c18aef84a598c3f6e1fab6b3489b84450ba08d9
MateaMilicevic <mateaaa.milicevic@gmail.com>
2018-05-15 14:05:23 +0200

    idemo dalje

---
+++ PiGrupa1/Controllers/SadnjaController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace PiGrupa1.Controllers
+{
+    public class SadnjaController : Controller
+    {
+        // GET
+        public ActionResult CreateSadnja()
+        {
+            return
+            View();
+        }
+    }
+}
---
+++ PiGrupa1/Controllers/ZivotinjeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace PiGrupa1.Controllers
+{
+    public class ZivotinjeController : Controller
+    {
+        // GET
+        public ActionResult CreateZivotinje()
+        {
+            return
+            View();
+        }
+    }
+}
