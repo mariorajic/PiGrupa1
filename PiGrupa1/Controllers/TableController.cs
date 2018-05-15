811baa3ea9f9d52fd248f42fd1fb2aa94060b8a9
mariorajic <mariorajic7@gmail.com>
2018-05-15 12:49:58 +0200

    table view & logo

---
+++ PiGrupa1/Controllers/TableController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace PiGrupa1.Controllers
+{
+    public class TableController : Controller
+    {
+        // GET
+        public ActionResult Table()
+        {
+            return
+            View();
+        }
+    }
+}
